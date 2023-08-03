90c52a6835de427f54e17e534db339e92bdbce10
Shaundi <Shaundiyoungshaundiyoung@gmail.com>
2023-08-03 15:44:16 -0500

    Finished Pseudo Travel Site

--- photos.js
+++ photos.js
@@ -15,3 +15,21 @@ function subtract (){
 
     }
 }
+
+
+
+function add1 (){
+    let amount = document.getElementById("amount1").innerText;
+    amount++;
+    console.log(amount); // 1,2,3...n times when button is
+    document.getElementById("amount1").innerText = amount;
+}
+
+function subtract1 (){
+    let amount = document.getElementById("amount1").innerText;
+    if (amount>0){
+    amount--;
+    document.getElementById("amount1").innerText = amount;
+
+    }
+}
